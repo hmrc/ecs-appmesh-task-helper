22abf226214f7d0c963bce65e003b11ca7497830
Steve Reddel <1107842+sreddel@users.noreply.github.com>
2023-07-25 16:29:09 +0100

    PBD-4284 have the helper healthcheck the service that is terminating for faster failure - update python version and dependencies

---
+++ task_helper/application_health_check.py
@@ -0,0 +1,20 @@
+import requests
+import logging
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.INFO)
+
+
+class ApplicationHealthCheck:
+    def __init__(self, port, path):
+        self.port = port
+        self.path = path
+
+    def is_healthy(self):
+        LOGGER.info("Checking the health of the app for startup issues")
+        url = f"http://127.0.0.1:{self.port}{self.path}"
+        try:
+            response = requests.post(url, timeout=2.5)
+        except requests.exceptions.RequestException:
+            return False
+        return True
