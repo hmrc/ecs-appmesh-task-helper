af9b54baf1ee3c92e3f9e3e09a92bd3f57dc130a
Steve Reddel <1107842+sreddel@users.noreply.github.com>
2023-07-25 16:29:09 +0100

    PBD-4284 have the helper healthcheck the service that is terminating for faster failure - update python version and dependencies

--- task_helper/environment_variables.py
+++ task_helper/environment_variables.py
@@ -8,8 +8,20 @@ class EnvironmentVariables:
     def get_drain_timeout(self) -> int:
         return self._get_integer_env_var("DRAIN_TIMEOUT", 80)
 
+    def get_port(self) -> str:
+        return self._get_string_env_var("PORT", "8080")
+
+    def get_path(self) -> str:
+        return self._get_string_env_var("HEALTHCHECK_PATH", "/ping/ping")
+
     def _get_integer_env_var(self, name, default) -> int:
         try:
             return int(str(os.getenv(name)))
         except Exception:
+            return default
+
+    def _get_string_env_var(self, name, default) -> str:
+        if os.getenv(name):
+            return str(os.getenv(name))
+        else:
             return default
--- task_helper/test_environment_variables.py
+++ task_helper/test_environment_variables.py
@@ -6,9 +6,7 @@ from environment_variables import EnvironmentVariables
 @pytest.fixture
 def environ(monkeypatch):
     # contains all the required environment variables.
-    data = {
-        "FOO": "60",
-    }
+    data = {"FOO": "60", "DOG": "WOOF"}
     for var_name, var_value in data.items():
         monkeypatch.setenv(var_name, var_value)
 
@@ -33,10 +31,26 @@ def test_get_drain_timeout_without_environment():
     assert environment_variables.get_drain_timeout() == 80
 
 
+def test_get_port_without_environment():
+    environment_variables = EnvironmentVariables()
+    assert environment_variables.get_port() == "8080"
+
+
+def test_get_path_without_environment():
+    environment_variables = EnvironmentVariables()
+    assert environment_variables.get_path() == "/ping/ping"
+
+
 @pytest.mark.usefixtures("environ")
 def test__get_integer_env_var_with_environment():
     environment_variables = EnvironmentVariables()
     assert environment_variables._get_integer_env_var("FOO", 40) == 60
+
+
+@pytest.mark.usefixtures("environ")
+def test__get_string_env_var_with_environment():
+    environment_variables = EnvironmentVariables()
+    assert environment_variables._get_string_env_var("DOG", "MOO") == "WOOF"
 
 
 @pytest.mark.usefixtures("environ_bad")
