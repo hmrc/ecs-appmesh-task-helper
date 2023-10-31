5b09edecc5fa6a651dce02a3be272087f02fe003
Steve Reddel <1107842+sreddel@users.noreply.github.com>
2023-10-31 16:03:40 +0000

    BAU remove application health check on shutdown due to 5xx errors

--- task_helper/main.py
+++ task_helper/main.py
@@ -22,29 +22,19 @@ async def sigterm_handler(loop):
     drain_delay = environment_variables.get_drain_delay()
     drain_timeout = environment_variables.get_drain_timeout()
 
-    application_health_check = ApplicationHealthCheck(
-        environment_variables.get_port(), environment_variables.get_path()
-    )
-    # when a service instance is starting but fails,
-    # we do not want to wait for the instance to drain.
-    if not application_health_check.is_healthy():
-        LOGGER.info("Service instance is not healthy. Draining is aborted")
-    else:
-        # This first delay is to allow new tasks to get up
-        # and running before killing the existing tasks
-        LOGGER.info(f"SIGTERM received, waiting for {drain_delay} seconds")
-        await asyncio.sleep(drain_delay)
+    # This first delay is to allow new tasks to get up
+    # and running before killing the existing tasks
+    LOGGER.info(f"SIGTERM received, waiting for {drain_delay} seconds")
+    await asyncio.sleep(drain_delay)
 
-        # Send the command to Envoy that effectively stops
-        # traffic going to the app
-        envoy_manager = EnvoyManager()
-        envoy_manager.healthcheck_fail()
+    # Send the command to Envoy that effectively stops
+    # traffic going to the app
+    envoy_manager = EnvoyManager()
+    envoy_manager.healthcheck_fail()
 
-        # Now allow existing requests to complete gracefully
-        LOGGER.info(
-            f"Waiting for {drain_timeout} seconds to allow for draining to finish"
-        )
-        await asyncio.sleep(drain_timeout)
+    # Now allow existing requests to complete gracefully
+    LOGGER.info(f"Waiting for {drain_timeout} seconds to allow for draining to finish")
+    await asyncio.sleep(drain_timeout)
 
     # Stopping the task here causes the cascading termination
     # process which is based on the container dependencies
